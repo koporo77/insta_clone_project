90b2549388b49e32ac7341e5e2e6a87ad1d5c837
koporo77 <guswns2847@gmail.com>
2020-08-18 20:55:48 +0900

    app bar adding icon

--- app/src/main/java/com/example/insta_copy/MainActivity.java
+++ app/src/main/java/com/example/insta_copy/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.insta_copy;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -16,16 +20,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard
-                , R.id.navigation_notifications , R.id.navigation_upload , R.id.navigation_profile)
+                , R.id.navigation_notifications, R.id.navigation_upload, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_item, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                Toast.makeText(this, "clicked heart", Toast.LENGTH_SHORT).show();
+
+                return true;
+            // ...
+            // ...
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
